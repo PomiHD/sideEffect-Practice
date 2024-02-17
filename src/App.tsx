f724e54cc8f304d08dd4d1573e942f9dc2a34b69
Samuel Yin <wsgddjy@live.com>
2024-02-18 00:47:28 +1000

    177. A Potential Problem with Side Effects: An Infinite Loop

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,14 @@ function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
-
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   navigator.geolocation.getCurrentPosition((position) => {
     const sortedPlaces = sortPlacesByDistance(
       AVAILABLE_PLACES,
       position.coords.latitude,
       position.coords.longitude,
     );
+    setAvailablePlaces(sortedPlaces);
   });
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -71,7 +72,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
